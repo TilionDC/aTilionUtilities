0decb5c6d3039cc2b39673be0c970907657c6970
TilionDC <TilionDC@gmail.com>
2016-06-28 11:28:31 +0200

    Added heal command. Seems it was omitted from the github pushes

---
+++ src/main/java/me/tiliondc/atu/modules/essentials/HealCommand.java
@@ -0,0 +1,67 @@
+package me.tiliondc.atu.modules.essentials;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HealCommand implements CommandExecutor {
+
+    JavaPlugin plugin;
+
+    public HealCommand(JavaPlugin plugin) {
+
+        this.plugin = plugin;
+
+        plugin.getCommand("Heal").setExecutor(this);
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        if(!(commandSender instanceof Player)) return false;
+        Player player = (Player) commandSender;
+
+        if(strings.length == 0) {
+            player.setHealth(20);
+            return true;
+        }
+
+        if(strings.length == 1) {
+            if(plugin.getServer().getPlayer(strings[0]) != null) {
+                Player p = plugin.getServer().getPlayer(strings[0]);
+                p.setHealth(20);
+                if(p.getMaxHealth() > 20) p.setMaxHealth(20);
+                return true;
+            } else {
+                double hearts = Integer.parseInt(strings[0]);
+                hearts = (hearts + player.getHealth()) > 20 ? 20 : hearts + player.getHealth();
+                player.setHealth(hearts);
+                if(player.getMaxHealth() > 20) player.setMaxHealth(20);
+                return true;
+            }
+
+        }
+
+        if(strings.length == 2 || strings.length == 3) {
+            double hearts = Integer.parseInt(strings[1]);
+            if(plugin.getServer().getPlayer(strings[0]) != null) {
+                Player p = plugin.getServer().getPlayer(strings[0]);
+                if(!commandSender.isOp() || strings.length == 2) {
+                    hearts = p.getHealth() + hearts > p.getMaxHealth() ? p.getMaxHealth() : hearts;
+                    p.setHealth(hearts);
+                    if(p.getMaxHealth() > 20) p.setMaxHealth(20);
+                } else if(commandSender.isOp()){
+                    player.setMaxHealth((hearts + player.getHealth()) > 20 ? player.getHealth() + hearts : 20);
+                    p.setHealth(hearts + player.getHealth());
+                }
+                return true;
+            }
+        }
+
+
+        return false;
+    }
+}
